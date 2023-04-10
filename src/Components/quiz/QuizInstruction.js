b33b645c7ae9b3da9c9d83bc5f4c253df7ff1f65
hanazein <hzain6097@gmail.com>
2023-04-10 17:35:33 +0200

    edit

--- src/Components/quiz/QuizInstruction.js
+++ src/Components/quiz/QuizInstruction.js
@@ -26,7 +26,9 @@ const QuizInstructions=()=>(
                 After finshing quiz you can solve it again if you want.
             </li>
         </ul>
-        <h2>Good Luck ..</h2><h2 className="p2">&#128512;</h2>
+        <h2>Good Luck ..</h2><h2 className="p2">&#128512;</h2><br/>
+        <Link to="/Exam" className='exm' >Go to Exam Page?</Link><br/>
+        <Link to="/" className='exm' >Back</Link>
     </div>
 </div>
 
