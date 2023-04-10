efcece85c18d0ba86664b0a3fbc7fc694281cffe
Mohamed Ali <74306747+mohamed7ali@users.noreply.github.com>
2023-04-10 03:06:37 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
+import Login from "./Pages/Sign_in";
+import SignUp from "./Pages/Sign_up";
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { BrowserRouter , Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+<BrowserRouter>
+<Routes>
+  <Route path='/'element={<Login />}> </Route>
+  <Route path='/signup'element={<SignUp />}> </Route>
+</Routes>
+</BrowserRouter>
+
 import "./App.css";
 
 
@@ -7,6 +23,7 @@ const App = () => {
     <h1>Hello</h1>
     
     </>
+
   );
 }
 
