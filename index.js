88c7c47a138b9f41141ab12fe115b9676aa84370
Mo3tsm <mo5les75291@gmail.com>
2023-04-28 14:33:08 +0300

    routes for admin were added and the index file updated

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const examQuestions = require("./routes/examQuestions");
 const user = require("./routes/users");
+const admin = require("./routes/admins");
+// const adminAuth = require("./middleware/adimnAuth");
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,8 +13,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //routes
-// app.use("", examQuestions);
+app.use("", examQuestions);
 app.use("", user);
+app.use("", admin);
 
 // Connection with server
 app.listen(4000, "localhost", () => {
