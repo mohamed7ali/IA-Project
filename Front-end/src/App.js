9b74180fbcb35efc649ab6d5adb28b24355c2e10
Mo3tsm <mo5les75291@gmail.com>
2023-04-30 18:24:06 +0300

    reorder

---
+++ Front-end/src/App.js
@@ -0,0 +1,29 @@
+import Login from "./Pages/Sign_in";
+import SignUp from "./Pages/Sign_up";
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { BrowserRouter , Routes, Route, Link } from "react-router-dom";
+import Main from "./Pages/Main";
+import QuizInstructions from "./Components/quiz/QuizInstruction";
+import { Quizh } from "./Components/quiz/quizH";
+
+function App() {
+  return (
+    
+    <BrowserRouter>
+<Routes>
+<Route path='/'element={<Main />}> </Route>
+ <Route path='/login'element={<Login />}> </Route>
+  <Route path='/signup'element={<SignUp />}> </Route>
+  <Route path='/QuizeInstruction'element={<QuizInstructions />}> </Route>
+ <Route path='/quiz'element={<Quizh/>}> </Route>
+</Routes>
+</BrowserRouter>
+    
+
+
+
+  )}
+
+export default App;
---
+++ Front-end/src/Pages/Contact_Us.js
@@ -0,0 +1,38 @@
+import "../Style_Pages/Contact_Us.css"
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import React from 'react';
+
+const Contact_Us = () => {
+    return (
+        <>
+        <Header />
+
+        <h1>Contact Us</h1>
+    <div class="container">
+      <h2>Get in touch</h2>
+      <form action="" method="post">
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name" placeholder="Your name.."/>
+
+        <label for="email">Email</label>
+        <input type="text" id="email" name="email" placeholder="Your email.."/>
+
+        <label for="subject">Subject</label>
+        <input type="text" id="subject" name="subject" placeholder="Subject.."/>
+
+        <label for="message">Message</label>
+        <textarea id="message" name="message" placeholder="Write your message here.."></textarea>
+
+        <input type="submit" value="Submit"/>
+      </form>
+      </div>
+
+      
+
+      <Footer />
+      </>
+    );
+};
+
+export default Contact_Us;
