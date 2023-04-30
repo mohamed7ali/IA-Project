cee54ba520336cf592cb96a6fb7ea8c6c738e908
hanazein <hzain6097@gmail.com>
2023-04-30 15:28:29 +0300

    delete

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import Header from "./Components/Header.js";
-import Main from "./Pages/Main.js";
-import App from "./App.js";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
