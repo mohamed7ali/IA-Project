e30137b95e7e717c147bb594b2c063db862b90bd
hanazein <hzain6097@gmail.com>
2023-05-07 17:34:59 +0300

    important

--- Back-end/routes/usersQueue.js
+++ Back-end/routes/usersQueue.js
@@ -16,48 +16,81 @@ router.get("/", (req, res) => {
 // Select a user by ID from user_queue and insert him into the user table, delete him from the user_queue table
 router.post("/:Id", async (req, res) => {
   const Id = req.params.Id;
-  try {
-    // Retrieve the user by ID from the user_queue table
-    connection.query(
-      "SELECT * FROM user_queue WHERE Id = ?",
-      [Id],
-      (error, rows, fields) => {
-        if (rows.length === 0) {
-          res.sendStatus(404);
+  connection.query(
+    "SELECT * FROM user_queue WHERE Id = ?",
+    [Id],
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        res.sendStatus(500);
+        return;
+      }
+
+      const user = results[0];
+
+      if (!user) {
+        return res.status(202).json({ message: "No users in queue" });
+      }
+
+      connection.beginTransaction((err) => {
+        if (err) {
+          console.error(err);
+          res.sendStatus(500);
           return;
         }
-        const user = rows[0];
-        // Insert the user into the user table
-        connection.query("INSERT INTO user set ?", {
-          Id: user.Id,
-          Name: user.Name,
-          Email: user.Email,
-          Phone: user.Phone,
-          Status: user.Status,
-          Password: user.Password,
-          verification_token: user.verification_token,
-        });
-        // delete the user from the user_queue
+
         connection.query(
-          "DELETE FROM user_queue WHERE Id = ?",
-          [Id],
-          (error, result, fields) => {
-            if (result.affectedRows === 0) {
-              res.sendStatus(404);
-            } else {
-              res.status(202).json({ message: "User deleted from the queue" });
+          "INSERT INTO user set ?",
+          {
+            Id: user.Id,
+            Name: user.Name,
+            Email: user.Email,
+            Phone: user.Phone,
+            Status: user.Status,
+            Password: user.Password,
+            verification_token: user.verification_token,
+          },
+          (err) => {
+            if (err) {
+              connection.rollback(() => {
+                console.error(err);
+                res.sendStatus(500);
+              });
+              return;
             }
+
+            connection.query(
+              "DELETE FROM user_queue WHERE id = ?",
+              [Id],
+              (err) => {
+                if (err) {
+                  connection.rollback(() => {
+                    console.error(err);
+                    res.sendStatus(500);
+                  });
+                  return;
+                }
+
+                connection.commit((err) => {
+                  if (err) {
+                    connection.rollback(() => {
+                      console.error(err);
+                      res.sendStatus(500);
+                    });
+                    return;
+                  }
+
+                  return res
+                    .status(202)
+                    .json({ message: `Processed user ${user.Id}` });
+                });
+              }
+            );
           }
         );
-        res.status(201).json({
-          message: `${user.Name} has been user registered`,
-        });
-      }
-    );
-  } catch (err) {
-    console.error(err);
-    res.sendStatus(500);
-  }
+      });
+    }
+  );
 });
 
 // Get a user by ID
